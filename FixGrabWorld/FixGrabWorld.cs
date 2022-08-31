f4fb21cc145c3522d5c90f4cc45e02091a23c84c
art0007i <art0007i@gmail.com>
2022-08-31 08:28:28 +0200

    Add project files.

---
+++ FixGrabWorld/FixGrabWorld.cs
@@ -0,0 +1,41 @@
+using HarmonyLib;
+using NeosModLoader;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using FrooxEngine;
+
+namespace FixGrabWorld
+{
+    public class FixGrabWorld : NeosMod
+    {
+        public override string Name => "FixGrabWorld";
+        public override string Author => "art0007i";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/art0007i/FixGrabWorld/";
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("me.art0007i.FixGrabWorld");
+            harmony.PatchAll();
+
+        }
+        [HarmonyPatch(typeof(GrabWorldLocomotion), "TryActivate")]
+        class FixGrabWorldPatch
+        {
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codes)
+            {
+                var lookfor = typeof(ILocomotionReference).GetMethod("get_DirectionReference");
+                foreach(var code in codes)
+                {
+                    if(code.operand == lookfor)
+                    {
+                        code.operand = typeof(ILocomotionReference).GetMethod("get_GripReference");
+                    }
+                    yield return code;
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# FixGrabWorld
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that fixes the grab world locomotion moving you forwards each time you use it.
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+1. Place [FixGrabWorld.dll](https://github.com/art0007i/FixGrabWorld/releases/latest/download/FixGrabWorld.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
